85c9ab71248ce3e0822d76acbe8f6a0b1bc52643
josias.rojas <josias.rojas@sansano.usm.cl>
2020-10-17 18:13:17 -0300

    basic cut

---
+++ October/Assets/Scripts/Cut.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cut : MonoBehaviour
+{
+    private bool cutting;
+    private bool cutMade;
+    private Renderer render;
+
+    private void Awake()
+    {
+        cutMade = false;
+        cutting = false;
+        render = gameObject.GetComponent<Renderer>();
+    }
+
+    private void OnMouseOver()
+    {
+        render.material.color = Color.blue;
+    }
+
+    private void OnMouseDown()
+    {
+        cutting = true;
+        cutMade = true;
+    }
+
+    private void OnMouseUp()
+    {
+        cutting = false;
+    }
+
+    private void OnMouseExit()
+    {
+        render.material.color = cutting ? Color.red : Color.green;
+        if (cutting)
+        {
+            Debug.Log("Wrong");
+        }
+        else if(cutMade)
+        {
+            Debug.Log("Good");
+            cutMade = false;
+        }
+    }
+}
