e512cdc2619ecfc9e8a1c08721bbf97226a50c60
josias.rojas <josias.rojas@sansano.usm.cl>
2020-10-17 18:29:25 -0300

    basic cut

--- October/Assets/Scripts/Cut.cs
+++ October/Assets/Scripts/Cut.cs
@@ -6,7 +6,10 @@ public class Cut : MonoBehaviour
 {
     private bool cutting;
     private bool cutMade;
+    private bool onLine;
     private Renderer render;
+    private Vector2 startPoint;
+    private Vector2 endPoint;
 
     private void Awake()
     {
@@ -17,22 +20,33 @@ public class Cut : MonoBehaviour
 
     private void OnMouseOver()
     {
+        onLine = true;
         render.material.color = Color.blue;
     }
 
     private void OnMouseDown()
     {
+        startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         cutting = true;
         cutMade = true;
     }
 
     private void OnMouseUp()
     {
+        endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (cutting && onLine) {
+            Debug.Log("Buena");
+            // 10 de size = 3 de maginitud
+            Vector2 diff = endPoint - startPoint;
+            Debug.Log(diff.magnitude);
+        }
         cutting = false;
     }
 
     private void OnMouseExit()
     {
+        
+        onLine = false;
         render.material.color = cutting ? Color.red : Color.green;
         if (cutting)
         {
