d8eeccea7cccb7977c97eee0be669df4d905c4f5
josias.rojas <josias.rojas@sansano.usm.cl>
2020-10-19 15:54:49 -0300

    Cambiar de escena al clickear un monstruo

---
+++ October/Assets/Scripts/Minijuego.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Minijuego : MonoBehaviour
+{
+    public int sceneIndex;
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ October/Assets/Scripts/TransicionNivel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransicionNivel : MonoBehaviour
+{
+    public Transform[] levelCenter;
+    private int level;
+
+    private void Awake()
+    {
+        level = 0;
+    }
+
+    public void MoveToNextPoint()
+    {
+        
+    }
+}
