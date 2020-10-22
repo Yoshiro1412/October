f45cb53c2f9f9c47c32d3a8ca28714905c2a205d
Matías Francia Carramiñana <matiasfrancia1@gmail.com>
2020-10-21 22:26:35 -0300

    escena 2

---
+++ October/Assets/Scripts/RotateCheck.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCheck : MonoBehaviour
+{
+    public GameObject checkpoint;
+    private bool count;
+    private float tiempo;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        count = false;
+        tiempo = 0f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(count) 
+        {
+            Debug.Log(tiempo);
+            tiempo += Time.deltaTime;
+            if(tiempo >= 10f) {
+                //Termina el juego
+                Debug.Log("Termino");
+                count = false;
+            }
+        } 
+        else
+        {
+            tiempo = 0f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Colisiono");
+        if(collision.gameObject == checkpoint)
+        {
+            count = true;
+            Debug.Log("Entro al checkpoint");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.gameObject == checkpoint) 
+        {
+            count = false;
+        }
+    }
+}
